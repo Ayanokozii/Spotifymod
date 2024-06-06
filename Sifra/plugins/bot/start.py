eb09360c15ee85c1fa94c89d78c911725850987f
Ayanokozii <ayanokozi0925@gmail.com>
2024-06-06 18:16:30 +0530

    Update start.py

--- Sifra/plugins/bot/start.py
+++ Sifra/plugins/bot/start.py
@@ -104,7 +104,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+        await message.reply_sticker("CAACAgQAAyEFAASDlIuSAAIFxWZhgVni6KTTV3WU8-zONSerHlGtAAI1FAACa9xwUPGDXwkw6k-5HgQ")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
