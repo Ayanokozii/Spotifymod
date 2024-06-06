730093afd77913f215685e41e457a9093142be73
Ayanokozii <ayanokozi0925@gmail.com>
2024-06-06 23:47:28 +0530

    Update Admin.py

--- Sifra/plugins/Anya/Admin.py
+++ Sifra/plugins/Anya/Admin.py
@@ -4,65 +4,149 @@ from pyrogram import *
 from pyrogram.types import *
 from Sifra.utils.admin_check import admin_filter
 
-strict_txt = [
-    "ᴅᴏsᴛ ᴋɪ ɢᴀᴀɴᴅ ɴʜɪ ᴍᴀᴀʀ sᴋᴛᴀ.",
-    "sᴀᴄʜ ᴍᴇ? ᴀᴘɴᴇ ᴅᴏsᴛ ᴋɪ ɢᴀᴀɴᴅ ᴍᴀᴀʀ ʟᴏᴏɴ.",
-    "ᴍᴀɪɴ ᴀᴘɴᴇ ʙᴇsᴛ ғʀɪᴇɴᴅ ᴋᴀ ʟᴜɴᴅ ɴʜɪ ᴋᴀᴀᴛ sᴋᴛᴀ.",
-    "ᴍᴀɪɴ ᴜsᴇ ᴊᴀᴀɴᴛᴀ ʜᴏᴏɴ. ᴍᴜᴊʜᴇ sᴀᴍᴀᴊʜɴᴇ ᴋᴀ ᴘʀʏᴀssʜ ᴋʀᴏ ɪsᴋɪ ᴍᴀᴀʀɪ ᴛᴏ ʀᴀɴᴅᴡᴀ ᴍᴀʀʀ ᴊᴀʏᴇɢᴀ."
+
+Love_text = [
+"hey please don't disturb me.",
+"who are you",    
+"aap kon ho",
+"aap mere owner to nhi lgte ",
+"hey tum mera name kyu le rhe ho meko sone do",
+"ha bolo kya kaam hai ",
+"dekho abhi mai busy hu ",
+"hey i am busy",
+"aapko smj nhi aata kya ",
+"leave me alone",
+"dude what happend",    
 ]
 
-promote = ["promote", "adminship"]
-fullpromote = ["fullpromote", "fadmin"]
-demote = ["demote", "lelo"]
+strict_txt = [
+"i can't restrict against my besties",
+"are you serious i am not restrict to my friends",
+"fuck you bsdk k mai apne dosto ko kyu kru",
+"hey stupid admin ", 
+"ha ye phele krlo maar lo ek dusre ki gwaand",  
+"i can't hi is my closest friend",
+"i love him please don't restict this user try to usertand "
+]
+
+
+ 
+ban = ["ban","boom"]
+unban = ["unban",]
+mute = ["mute","silent","shut"]
+unmute = ["unmute","speak","free"]
+kick = ["kick", "out","nikaal","nikal"]
+promote = ["promote","adminship"]
+demote = ["demote","lelo"]
+group = ["group"]
+channel = ["channel"]
+
+
 
 # ========================================= #
 
-@app.on_message(filters.command(["ifra","uvi","tar], prefixes=["S", "S", "S"]) & admin_filter)
-async def handle_admin_commands(_, message):
+
+@app.on_message(filters.command(["ifra","uvi","tar"], prefixes=["S", "S", "S"]) & admin_filter)
+async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
+    if len(message.text) < 2:
+        return await message.reply(random.choice(Love_text))
+    bruh = message.text.split(maxsplit=1)[1]
+    data = bruh.split(" ")
     
-    if not reply:
-        return await message.reply(random.choice(strict_txt))
+    if reply:
+        user_id = reply.from_user.id
+        for banned in data:
+            print(f"present {banned}")
+            if banned in ban:
+                if user_id in SUDOERS:
+                    await message.reply(random.choice(strict_txt))          
+                else:
+                    await app.ban_chat_member(chat_id, user_id)
+                    await message.reply("OK, Ban kar diya madrchod ko sala Chutiya tha !")
+                    
+        for unbanned in data:
+            print(f"present {unbanned}")
+            if unbanned in unban:
+                await app.unban_chat_member(chat_id, user_id)
+                await message.reply(f"Ok, aap bolte hai to unban kar diya") 
+                
+        for kicked in data:
+            print(f"present {kicked}")
+            if kicked in kick:
+                if user_id in SUDOERS:
+                    await message.reply(random.choice(strict_txt))
+                
+                else:
+                    await app.ban_chat_member(chat_id, user_id)
+                    await app.unban_chat_member(chat_id, user_id)
+                    await message.reply("get lost! bhga diya bhosdi wale ko") 
+                    
+        for muted in data:
+            print(f"present {muted}") 
+            if muted in mute:
+                if user_id in SUDOERS:
+                    await message.reply(random.choice(strict_txt))
+                
+                else:
+                    permissions = ChatPermissions(can_send_messages=False)
+                    await message.chat.restrict_member(user_id, permissions)
+                    await message.reply(f"muted successfully! Disgusting people.") 
+                    
+        for unmuted in data:
+            print(f"present {unmuted}")            
+            if unmuted in unmute:
+                permissions = ChatPermissions(can_send_messages=True)
+                await message.chat.restrict_member(user_id, permissions)
+                await message.reply(f"Huh, OK, sir!")   
 
-    user_id = reply.from_user.id
-    command = message.command[0].lower()
 
-    if command in promote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=False,
-            can_invite_users=True,
-            can_delete_messages=True,
-            can_restrict_members=False,
-            can_pin_messages=True,
-            can_promote_members=False,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-        ))
-        await message.reply("♡ sᴜᴄᴄᴇssғᴜʟʟʏ ᴘʀᴏᴍᴏᴛᴇᴅ!")
+        for promoted in data:
+            print(f"present {promoted}")            
+            if promoted in promote:
+                await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+                    can_change_info=False,
+                    can_invite_users=True,
+                    can_delete_messages=True,
+                    can_restrict_members=False,
+                    can_pin_messages=True,
+                    can_promote_members=False,
+                    can_manage_chat=True,
+                    can_manage_video_chats=True,
+                       )
+                     )
+                await message.reply("promoted !")
 
-    elif command in demote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=False,
-            can_invite_users=False,
-            can_delete_messages=False,
-            can_restrict_members=False,
-            can_pin_messages=False,
-            can_promote_members=False,
-            can_manage_chat=False,
-            can_manage_video_chats=False,
-        ))
-        await message.reply("♡ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴍᴏᴛᴇᴅ!")
+        for demoted in data:
+         print(f"present {demoted}")            
+            if demoted in demote:
+                await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+                    can_change_info=False,
+                    can_invite_users=False,
+                    can_delete_messages=False,
+                    can_restrict_members=False,
+                    can_pin_messages=False,
+                    can_promote_members=False,
+                    can_manage_chat=False,
+                    can_manage_video_chats=False,
+                       )
+                     )
+                await message.reply("demoted !")
 
-    elif command in fullpromote:
-        await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
-            can_change_info=True,
-            can_invite_users=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_pin_messages=True,
-            can_promote_members=True,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-        ))
+        for fullpromote in data:
+         print(f"present {demoted}")
+            if fullpromoted in promote:
+                await app.promote_chat_member(chat_id, user_id, privileges=ChatPrivileges(
+                    can_change_info=True,
+                    can_invite_users=True,
+                    can_delete_messages=True,
+                    can_restrict_members=True,
+                    can_pin_messages=True,
+                    can_promote_members=True,
+                    can_manage_chat=True,
+                    can_manage_video_chats=True,
+                           )
+                         )
         await message.reply("♡ sᴜᴄᴄᴇssғᴜʟʟʏ ғᴜʟʟ ᴘʀᴏᴍᴏᴛᴇᴅ!")
+
